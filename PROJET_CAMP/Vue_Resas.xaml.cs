455edf50e5b83c8f81214d311f564adb695049b4
nivunico <zonca.nicolas@gmail.com>
2024-02-09 14:00:22 +0100

    Ajoutez des fichiers projet.

---
+++ PROJET_CAMP/Vue_Resas.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PROJET_CAMP
+{
+    /// <summary>
+    /// Logique d'interaction pour Vue_Resas.xaml
+    /// </summary>
+    public partial class Vue_Resas : Window
+    {
+        public Vue_Resas()
+        {
+            InitializeComponent();
+        }
+    }
+}
